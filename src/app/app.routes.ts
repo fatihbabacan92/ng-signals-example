1c4312a0f72bac076a27b002400bc68475985435
Fatih <fatihb@live.be>
2025-11-11 14:55:24 +0100

    Added Discover Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { DiscoverComponent } from './features/discover/discover.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'discover', pathMatch: 'full' },
+  { path: 'discover', component: DiscoverComponent },
+  // watchlist & favorites pages will go here later
+];
---
+++ src/app/features/discover/discover.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, effect } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LibraryService } from '../../shared/services/library.service';
+import { GhibliApiService } from '../../shared/api/ghibli-api.service';
+
+@Component({
+  selector: 'app-discover',
+  standalone: true,
+  templateUrl: './discover.component.html',
+  styleUrls: ['./discover.component.scss'],
+  imports: [CommonModule],
+})
+export class DiscoverComponent implements OnInit {
+  logFilms = effect(() => {
+    console.log('Number of films:', this.ghibliApi.films().length);
+  });
+
+  constructor(public ghibliApi: GhibliApiService, public library: LibraryService) {}
+
+  ngOnInit() {
+    if (!this.ghibliApi.films().length) {
+      this.ghibliApi.loadFilms();
+    }
+  }
+}
