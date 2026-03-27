4f5bbb573b00d538876f972160f628e241108e93
Fatih <fatihb@live.be>
2026-03-27 22:02:28 +0100

    UI Improvements

--- src/app/shared/interfaces/film.interface.ts
+++ src/app/shared/interfaces/film.interface.ts
@@ -1,7 +1,13 @@
 export interface Film {
   id: string;
   title: string;
+  original_title: string;
   description: string;
+  director: string;
+  producer: string;
   release_date: string;
-  image?: string;
+  running_time: string;
+  rt_score: string;
+  image: string;
+  movie_banner: string;
 }
